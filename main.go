e8246d1dcf377df654186dfe95d750a882c90f55
LanceHE6 <2765543491@qq.com>
2024-09-14 22:33:20 +0800

    feat:实现数据库初始化

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"WorkProgressRecord/util"
+	"WorkProgressRecord/util/config"
+	"WorkProgressRecord/util/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	ginServer := gin.Default()
-
+	// 跨域
 	ginServer.Use(util.Cors())
-
-	err := ginServer.Run("8070")
+	// 初始化数据库
+	db.Init()
+	err := ginServer.Run(":" + config.ServerConfig.SERVER.PORT)
 	if err != nil {
 		return
 	}
