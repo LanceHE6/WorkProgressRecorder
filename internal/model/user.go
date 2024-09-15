1f51891390d2d194f8abe1d77dbfa0aa8071f917
LanceHE6 <2765543491@qq.com>
2024-09-15 22:00:54 +0800

    chore:account字段增加unique约束

--- internal/model/user.go
+++ internal/model/user.go
@@ -5,9 +5,9 @@ package model
 //	@Description: 用户表结构
 type User struct {
 	Base
-	Account    string `gorm:"column:account;type:varchar(255);not null" json:"account"` // 账号
-	Password   string `gorm:"column:password;type:varchar(255);not null" json:"-"`      // 密码
-	Name       string `gorm:"column:name;type:varchar(255);not null" json:"name"`       // 姓名
-	Permission int    `gorm:"column:permission;type:int;not null" json:"permission"`    // 权限
-	SessionID  string `gorm:"column:session_id;type:varchar(255)" json:"-"`             // session_id
+	Account    string `gorm:"column:account;type:varchar(255);not null;unique" json:"account"` // 账号
+	Password   string `gorm:"column:password;type:varchar(255);not null" json:"-"`             // 密码
+	Name       string `gorm:"column:name;type:varchar(255);not null" json:"name"`              // 姓名
+	Permission int    `gorm:"column:permission;type:int;not null" json:"permission"`           // 权限
+	SessionID  string `gorm:"column:session_id;type:varchar(255)" json:"-"`                    // session_id
 }
