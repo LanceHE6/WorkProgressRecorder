704f1c350235325604d38621c9b57e54bc0e09da
LanceHE6 <2765543491@qq.com>
2024-09-15 22:00:25 +0800

    chore:导出jwtkey

--- pkg/jwt.go
+++ pkg/jwt.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-var jwtKey = []byte(config.ServerConfig.SERVER.SECRET_KEY) // 用于签名的密钥
+var JwtKey = []byte(config.ServerConfig.SERVER.SECRET_KEY) // 用于签名的密钥
 
 // MyClaims 自定义载荷内容
 type MyClaims struct {
@@ -39,7 +39,7 @@ func GenerateToken(id int64, permission int, name, sessionID string) (string, er
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtKey)
+	tokenString, err := token.SignedString(JwtKey)
 
 	if err != nil {
 		return "", err
