fc4ce040ff8829976d6c5782d431181dd2f2eaf3
LanceHE6 <2765543491@qq.com>
2024-09-18 10:10:27 +0800

    feat:新增打卡表

---
+++ internal/model/clock_in.go
@@ -0,0 +1,10 @@
+package model
+
+type ClockIn struct {
+	Base
+	// 关联用户
+	User            *User  `gorm:"foreignKey:UID" json:"-"`
+	UID             int64  `json:"uid"`
+	ClockInTime     int64  `json:"clock_in_time"`     // 打卡时间戳
+	ClockInLocation string `json:"clock_in_location"` // 打卡地点
+}
