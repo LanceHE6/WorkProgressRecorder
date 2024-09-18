4018e08b10b0ccef78f71ac0f1908709bb086047
CM-Silence <757902252@qq.com>
2024-09-18 15:24:59 +0800

    [config]环境配置 [new]创建所需要的视图

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+import { router } from './router/index'
+import axios from "axios"
 import './style.css'
 import App from './App.vue'
+import naive from 'naive-ui'
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.provide("axios", axios)
+
+app.use(router)
+    .use(naive)
+
+app.mount('#app')
---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <n-button
+    type="primary"
+  >
+    this is a button
+  </n-button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
