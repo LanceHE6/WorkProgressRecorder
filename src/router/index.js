6cee7eab6a4ce33d97fdcc69242ab0008a8d1dac
CM-Silence <757902252@qq.com>
2024-09-21 23:19:15 +0800

    [feat]更新页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Main from '../views/Main.vue';
-
+import Postgraduate from "../views/Postgraduate.vue";
+import Occupation from "../views/Occupation.vue";
+import UserCenter from "../views/UserCenter.vue";
+import LogDetail from "../views/LogDetail.vue";
+import UserManagement from "../views/UserManagement.vue";
 
 const routes = [
-    { path: '/', component: Main , name: 'main'},
+    { path: '/', component: Main , name: 'main', meta:{index:0},},
+    { path: '/postgraduate', component: Postgraduate , name: 'postgraduate', meta:{index:1},},
+    { path: '/occupation', component: Occupation , name: 'occupation', meta:{index:1},},
+    { path: '/logDetail', component: LogDetail , name: 'logDetail', meta:{index:2},},
+    { path: '/userCenter', component: UserCenter , name: 'userCenter', meta:{index:1},},
+    { path: '/userManagement', component: UserManagement , name: 'userManagement', meta:{index:1},},
 ];
 
 const router = createRouter({
