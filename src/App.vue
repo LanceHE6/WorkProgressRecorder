6e19cf05b8638ce4a1339d11c67d319234ee96de
CM-Silence <757902252@qq.com>
2024-09-21 23:18:05 +0800

    [config]更新App.vue配置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script setup>
+import {ref} from "vue";
+import {useRoute} from "vue-router";
+import { dateZhCN, zhCN } from 'naive-ui'
+
+const transitionName = ref('')
+const route = useRoute();
 
 </script>
 
 <template>
-  <n-config-provider :theme-overrides="{ common: { fontWeightStrong: '600' } }">
-    <router-view/>
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+    <n-dialog-provider>
+      <n-message-provider>
+        <router-view v-slot="{ Component }">
+          <!--        <keep-alive>-->
+          <component :is="Component"/>
+          <!--        </keep-alive>-->
+        </router-view>
+      </n-message-provider>
+    </n-dialog-provider>
   </n-config-provider>
 </template>
 
 <style scoped>
+
 </style>
