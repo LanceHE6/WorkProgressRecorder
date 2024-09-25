3397b4e9a7c1ccc4a32e3b646be655d95a27ac05
CM-Silence <757902252@qq.com>
2024-09-25 22:39:06 +0800

    [feat]现在用户可以在考研打卡页面查询自己的历史打卡记录了

--- src/views/Postgraduate.vue
+++ src/views/Postgraduate.vue
@@ -38,6 +38,56 @@
         {{ isCheck ? '已打卡' : '学习打卡' }}
       </n-button>
     </n-row>
+    <n-row style="margin-top: 10px">
+      <n-date-picker
+          v-model:formatted-value="checkinListParams.date"
+          value-format="yyyy-MM-dd"
+          type="date"
+          placeholder="按日期查询"
+          clearable
+          style="width: 35%; margin-right: 15px; text-align: start"
+      >
+
+      </n-date-picker>
+      <n-select
+          v-model:value="checkinListParams.time_slot"
+          placeholder="按时间段查询"
+          :options="[
+            {label: '上午', value: '上午'},
+            {label: '下午', value: '下午'},
+            {label: '晚上', value: '晚上'},
+          ]"
+          clearable
+          style="width: 35%; margin-right: 30px; text-align: start"
+      >
+
+      </n-select>
+      <n-button
+        type="primary"
+        @click="getCheckinList"
+        secondary
+      >
+         搜索
+      </n-button>
+    </n-row>
+    <n-row style="justify-content: center; margin-top: 10px">
+      <n-data-table
+          :columns="checkinHead"
+          :data="checkinList"
+          :bordered="false"
+          max-height="40vh"
+          style="width: 100%; height: 40vh; background-color: #f9f9f9c0;"
+      />
+    </n-row>
+    <n-pagination
+        :display-order="['pages', 'quick-jumper']"
+        :item-count="total"
+        :page-sizes="[pageSize]"
+        :page-slot="5"
+        show-quick-jumper
+        style="margin-top: 10px"
+        :on-update-page="checkinPageChange"
+    />
   </div>
 
   <n-modal
@@ -73,9 +123,11 @@ import {GolfOutline} from '@vicons/ionicons5'
 import {router} from "../router/index.js";
 import {onMounted, ref} from "vue";
 import {axiosGet, axiosPost} from "../utils/axiosUtil.js";
-import {useMessage} from "naive-ui";
+import {NButton, useMessage} from "naive-ui";
+import {getUser} from "../utils/appManager.js";
 
 const message = useMessage()
+const pageSize = 10
 
 const examDay = ref(null)
 const goal = ref(null)
@@ -84,6 +136,14 @@ const showCheckInModal = ref(false)
 const isCheck = ref(true)
 
 const place = ref(null)
+const total = ref(0)
+const checkinList = ref([])
+const checkinListParams = ref({
+  uid: '',
+  date: null,
+  time_slot: null,
+  page: 1,
+})
 
 function back(){
   router.push("/")
@@ -97,10 +157,22 @@ const options = [
   {label: '信科楼', value: '信科楼'},
 ]
 
+const checkinHead = [
+  {title: "序号", key: "number",
+    render: (_, index) => {
+      return `${index + 1}`;
+    }
+  },
+  { title: "打卡地点", key: "clock_in_location"},
+  { title: "打卡时间", key: "clock_in_time", render: (row) => {
+      return new Date(row.clock_in_time * 1000).toLocaleString()
+    }},
+]
+
 onMounted(async () =>{
   const result = await axiosGet({
     url: '/user/target',
-    name: 'post-get-empl_goal'
+    name: 'postList-get-empl_goal'
   })
   if(result && result.data && result.data.direction.includes(1)){
     goal.value = result.data.goal.pg_goal
@@ -111,7 +183,7 @@ onMounted(async () =>{
 
   const result2 = await axiosGet({
     url: '/clock_in/is_clock_in',
-    name: 'post-get-clock'
+    name: 'postList-get-clock'
   })
   if(result2){
     if(result2.code === 0){
@@ -127,7 +199,7 @@ onMounted(async () =>{
 
   const result3 = await axiosGet({
     url: '/pgee/cd',
-    name: 'post-get-exam-day'
+    name: 'postList-get-exam-day'
   })
   if(result3 && result3.data){
     examDay.value = result3.data.days
@@ -135,7 +207,28 @@ onMounted(async () =>{
   else{
     message.error("考研时间获取失败")
   }
+  await getCheckinList()
 })
+
+async function getCheckinList(){
+  checkinListParams.value.uid = getUser().id
+  if(!checkinListParams.value.uid){
+    message.error("身份验证失败，请重新登录")
+    return
+  }
+  const result = await axiosGet({
+    url: '/clock_in/search',
+    params: checkinListParams.value,
+    name: 'get-checkin-list'
+  })
+  if(result && result.data){
+    checkinList.value = result.data.rows
+    total.value = result.data.total
+  }
+  else{
+    message.error("打卡记录获取失败")
+  }
+}
 
 async function checkIn(){
   if(!place.value){
@@ -152,6 +245,7 @@ async function checkIn(){
   if(result){
     if(result.data.code === 0){
       message.success("打卡成功")
+      await getCheckinList()
       isCheck.value = true
     }
     else{
@@ -163,19 +257,14 @@ async function checkIn(){
   }
 }
 
+async function checkinPageChange(page){
+  checkinListParams.value.page = page
+  await getCheckinList()
+}
 
 </script>
 
 <style scoped>
-.main-body{
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start; /* 子元素在父容器中垂直分布 */
-  width: 100%;
-  height: 100%;
-  padding: 15px 10px 10px 10px;
-  box-sizing: border-box;
-}
 .profile-title-card{
   margin: 30px 0 0 0;
   width: 95%;
