4f8d1160a9c20ad8593fa4d573ce21c2b4163131
LanceHE6 <2765543491@qq.com>
2025-03-18 15:23:55 +0800

    perf(post): 优化帖子查询逻辑
    - 移除了对 anonymous 字段的判断，简化了查询条件
    - 提高了查询效率，特别是在用户未选择匿名发帖的情况下

--- internal/repo/impl/post_repo_impl.go
+++ internal/repo/impl/post_repo_impl.go
@@ -67,7 +67,7 @@ func (p PostRepoImpl) Search(params pkg.SearchPostsParams) ([]model.Post, int, e
 	query := p.modelDB().Preload("User")
 	// 构建查询
 	if params.UID != 0 {
-		query = query.Where("uid = ? AND anonymous IS FALSE", params.UID)
+		query = query.Where("uid = ?", params.UID)
 	}
 	// 计算总数
 	var count int
